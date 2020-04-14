d50f3afc5bcd8b730841e76874c22179e6ae7fbf
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-14 10:05:44 +0200

    Upgrade: Change references from teecke to tpbtools

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -2,6 +2,6 @@
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - Nexus Service (c) Teecke 2019-2020"
+echo "Generic Platform - Nexus Service (c) 2020 TIC para Bien"
 echo
 
